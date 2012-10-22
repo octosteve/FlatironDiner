678984379a960dc04d50a3fc5cbe9b9e0fceb8e6
Steven Nunez <steven.nunez@gmail.com>
2012-10-22 19:45:42 -0400

    Got rid of #respond_to checks. Methods being used in other tests provide a good check already

--- spec/FlatironDiner/menu_item_spec.rb
+++ spec/FlatironDiner/menu_item_spec.rb
@@ -2,20 +2,10 @@ require 'spec_helper'
 
 describe MenuItem do 
 
-  it "responds to #name" do 
-    menu_item = MenuItem.new
-    menu_item.should respond_to(:name)
-  end
-
   it "can have its name set" do 
     menu_item = MenuItem.new
     menu_item.name = "Chunky Bacon"
     menu_item.name.should == "Chunky Bacon"
-  end
-
-  it "responds to #price" do
-    menu_item = MenuItem.new
-    menu_item.should respond_to(:price)
   end
 
   it "can have its price set" do
@@ -24,20 +14,10 @@ describe MenuItem do
     menu_item.price.should == "2.99"
   end
 
-  it "responds to #calorie_count" do
-    menu_item = MenuItem.new
-    menu_item.should respond_to(:calorie_count)
-  end
-
   it "can have its calorie count set" do 
     menu_item = MenuItem.new
     menu_item.calorie_count = 256
     menu_item.calorie_count.should == 256
-  end
-
-  it "responds to #ingredients" do
-    menu_item = MenuItem.new
-    menu_item.should respond_to(:ingredients)
   end
 
   it "can have its ingredients set" do 
