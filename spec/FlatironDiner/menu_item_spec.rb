1b134d48537bab71c74a31779af88f81d189c268
Steven Nunez <steven.nunez@gmail.com>
2012-10-22 19:43:12 -0400

    Add a bunch of failing tests. They are all part of the same feature IMHO

--- spec/FlatironDiner/menu_item_spec.rb
+++ spec/FlatironDiner/menu_item_spec.rb
@@ -10,5 +10,39 @@ describe MenuItem do
   it "can have its name set" do 
     menu_item = MenuItem.new
     menu_item.name = "Chunky Bacon"
+    menu_item.name.should == "Chunky Bacon"
+  end
+
+  it "responds to #price" do
+    menu_item = MenuItem.new
+    menu_item.should respond_to(:price)
+  end
+
+  it "can have its price set" do
+    menu_item = MenuItem.new
+    menu_item.price = "2.99"
+    menu_item.price.should == "2.99"
+  end
+
+  it "responds to #calorie_count" do
+    menu_item = MenuItem.new
+    menu_item.should respond_to(:calorie_count)
+  end
+
+  it "can have its calorie count set" do 
+    menu_item = MenuItem.new
+    menu_item.calorie_count = 256
+    menu_item.calorie_count.should == 256
+  end
+
+  it "responds to #ingredients" do
+    menu_item = MenuItem.new
+    menu_item.should respond_to(:ingredients)
+  end
+
+  it "can have its ingredients set" do 
+    menu_item = MenuItem.new
+    menu_item.ingredients = [:bacon, :thats_it]
+    menu_item.ingredients.should == [:bacon, :thats_it]
   end
 end
