79dba3bd4c1886d4b33ba1fcc0f69e1168a27fba
Steven Nunez <steven.nunez@gmail.com>
2012-10-22 19:47:18 -0400

    Use rspec's subject to reduce code noise

--- spec/FlatironDiner/menu_item_spec.rb
+++ spec/FlatironDiner/menu_item_spec.rb
@@ -3,26 +3,22 @@ require 'spec_helper'
 describe MenuItem do 
 
   it "can have its name set" do 
-    menu_item = MenuItem.new
-    menu_item.name = "Chunky Bacon"
-    menu_item.name.should == "Chunky Bacon"
+    subject.name = "Chunky Bacon"
+    subject.name.should == "Chunky Bacon"
   end
 
   it "can have its price set" do
-    menu_item = MenuItem.new
-    menu_item.price = "2.99"
-    menu_item.price.should == "2.99"
+    subject.price = "2.99"
+    subject.price.should == "2.99"
   end
 
   it "can have its calorie count set" do 
-    menu_item = MenuItem.new
-    menu_item.calorie_count = 256
-    menu_item.calorie_count.should == 256
+    subject.calorie_count = 256
+    subject.calorie_count.should == 256
   end
 
   it "can have its ingredients set" do 
-    menu_item = MenuItem.new
-    menu_item.ingredients = [:bacon, :thats_it]
-    menu_item.ingredients.should == [:bacon, :thats_it]
+    subject.ingredients = [:bacon, :thats_it]
+    subject.ingredients.should == [:bacon, :thats_it]
   end
 end
