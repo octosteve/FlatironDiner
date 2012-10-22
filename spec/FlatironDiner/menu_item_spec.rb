ae1f7005484a4ecc32428949036fed181d8d5c6b
Steven Nunez <steven.nunez@gmail.com>
2012-10-22 19:36:45 -0400

    Feature for free! Struct ftw

--- spec/FlatironDiner/menu_item_spec.rb
+++ spec/FlatironDiner/menu_item_spec.rb
@@ -6,4 +6,9 @@ describe MenuItem do
     menu_item = MenuItem.new
     menu_item.should respond_to(:name)
   end
+
+  it "can have its name set" do 
+    menu_item = MenuItem.new
+    menu_item.name = "Chunky Bacon"
+  end
 end
