c4af2c3715171e57a7faac682baef9d57b6f39f4
Steven Nunez <steven.nunez@gmail.com>
2012-10-23 18:13:48 -0400

    Add #show test to return current "cart"

--- spec/FlatironDiner/order_spec.rb
+++ spec/FlatironDiner/order_spec.rb
@@ -8,5 +8,21 @@ describe Order do
 
     order.items.should include bacon
   end
+
+  it "keeps track of item count" do 
+    bacon = MenuItem.new
+    bacon.name = "Bacon"
+
+    eggs = MenuItem.new
+    eggs.name = "Eggs"
+
+    order = Order.new
+
+    order.add bacon
+    order.add eggs
+    order.add eggs
+
+    order.show.should == ["Bacon x1", "Eggs x2"]
+  end
 end
 
