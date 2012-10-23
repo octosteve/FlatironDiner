b8b67a802abf7d1405dd22c418dc214508b2b999
Steven Nunez <steven.nunez@gmail.com>
2012-10-22 20:28:00 -0400

    FINALLY! Write some REAL code man!

--- spec/FlatironDiner/menu_spec.rb
+++ spec/FlatironDiner/menu_spec.rb
@@ -16,5 +16,23 @@ describe Menu do
     menu = Menu.new
     menu.show.should == []
   end
+
+  it "can have multiple items in it" do
+    menu_item = MenuItem.new 
+    menu_item.name = "Awesome Hotdogs"
+    menu_item.price = "2.99"
+
+    menu_item2 = MenuItem.new 
+    menu_item2.name = "Chunky Bacon"
+    menu_item2.price = "0.99"
+
+    menu = Menu.new
+
+    menu.add menu_item
+    menu.add menu_item2
+
+    result = ["1.Awesome Hotdogs, only $2.99!", "2. Chunky Bacon, only $0.99!"]
+    menu.show.should == result
+  end
 end
 
