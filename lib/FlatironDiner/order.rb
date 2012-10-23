cfde0c66a459445b5777d71eca120a5776c73233
Steven Nunez <steven.nunez@gmail.com>
2012-10-22 21:06:57 -0400

    Order add implemented

---
+++ lib/FlatironDiner/order.rb
@@ -0,0 +1,12 @@
+class Order
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+  def add(item)
+    items << item
+  end
+
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,5 @@
 $:<< File.join(File.dirname(__FILE__), '..')
 require 'FlatironDiner/menu_item'
 require 'FlatironDiner/menu'
+require 'FlatironDiner/order'
 
