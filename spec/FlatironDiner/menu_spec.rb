a87d5b1cbcebb359cb6f1bc9c82255edadcac305
Steven Nunez <steven.nunez@gmail.com>
2012-10-22 20:06:00 -0400

    Roadblocked by #add

--- spec/FlatironDiner/menu_spec.rb
+++ spec/FlatironDiner/menu_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe Menu do 
+  it "can have MenuItems added to it" do 
+    menu_item = MenuItem.new 
+    menu_item.name = "Awesome Hotdogs"
+    menu_item.price = "2.99"
+
+    menu = Menu.new
+
+    menu.add menu_item
+  end
 
 end
 
