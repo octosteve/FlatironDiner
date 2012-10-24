d94298b5272066609e9bcf982a36025d4b8c51c8
Steven Nunez <steven.nunez@gmail.com>
2012-10-24 08:51:11 -0400

    Implemented Recomender!

--- lib/FlatironDiner/recommender.rb
+++ lib/FlatironDiner/recommender.rb
@@ -5,6 +5,17 @@ class Recommender
   end
 
   def recomendations
-    [@menu.fetch(2)]
+    find_similar_items
+  end
+
+  private
+
+  def find_similar_items
+    @similar = @order.menu_items.collect do |order_mi|
+      @menu.menu_items.collect do |menu_mi|
+        next if menu_mi == order_mi
+        menu_mi unless (order_mi.ingredients & menu_mi.ingredients).empty?
+      end
+    end.flatten.compact
   end
 end
--- spec/FlatironDiner/recommender_spec.rb
+++ spec/FlatironDiner/recommender_spec.rb
@@ -1,6 +1,26 @@
 require 'spec_helper'
 
 describe Recommender do 
+  it "returns an empty array if there are no matches" do
+    menu = Menu.new
+
+    bacon = MenuItem.new
+    bacon.name = "Chunky Bacon"
+    bacon.ingredients = [:bacon]
+
+    veggie_burger = MenuItem.new
+    veggie_burger.name = "Brocolli"
+    veggie_burger.ingredients = [:veggies, :unspeakable]
+                
+    [bacon, veggie_burger].each {|item| menu.add item}
+    order = Order.new
+
+    order.add bacon
+
+    recommender = Recommender.new menu: menu, order: order
+
+    recommender.recomendations.should be_empty
+  end
   it "returns a list of items with similar ingredients" do
     menu = Menu.new
 
